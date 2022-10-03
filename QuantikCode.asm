6e9f7b5865c1b2169c95d8e257b06f1e75e01319
David Fernandez <dfernandezs2004@gmail.com>
2022-10-03 11:26:16 -0600

    Crear Ready

--- QuantikCode.asm
+++ QuantikCode.asm
@@ -37,7 +37,7 @@ datos segment
     msgNoSobrescribir    db "No se va a sobrescribir el tablero",10,13,'$'
     msgTableroCreado     db "No se va a sobrescribir el tablero",10,13,'$'
     msgErrorCrearTablero db "Hubo un error en la creacion del tablero",10,13,'$'
-    inputArchivo         db "Ya existe tablero, si quiere reiniciarlo opima S, sino cualquier tecla",10,13,'$'
+    inputArchivo         db "Ya existe tablero, si quiere reiniciarlo opima Y, sino cualquier tecla",10,13,'$'
 
     enterMsg             db " ",10,13,'$'
 
@@ -58,39 +58,6 @@ enterCall proc near
                          ret
 enterCall endP
 
-crearTablero proc near
-    
-    createFile1:         mov    ah, 3CH                       ;Crea un nuevo archivo
-                         xor    cx, cx
-                         lea    dx, nombArchivo
-                         int    21h
-                         jc     errorTablero
-
-    generalAddBuffer:    mov    handleS, ax
-                       
-                         mov    ah, 40h                       ;Se agrega la data normal
-                         mov    cx,19
-                         lea    dx, bufferTablero
-                         int    21h
-
-    cerrarFile1:         mov    ah, 3Eh                       ; cerrar el archivo cesar
-                         mov    bx, handleS
-                         int    21h
-
-                         mov    ah, 09h
-                         lea    dx, msgTableroCreado
-                         int    21h
-
-                         jmp    terminar
-                                         
-    errorTablero:        lea    dx, msgErrorCrearTablero
-                         mov    ah, 09h
-                         int    21h
-
-    terminar:            ret
-                      
-crearTablero endp
-
 verificarTablero proc near
                          lea    dx,nombArchivo
                          mov    ax,3D00h
@@ -103,7 +70,8 @@ verificarTablero proc near
                          int    21h
                          jc     errorSobreEcribir
                          mov    handleS,ax
-    ;************************************************************
+                         jmp    escribirTableroNuevo
+   
     existenteError:      
                          mov    ah, 09h
                          lea    dx, inputArchivo
@@ -111,8 +79,8 @@ verificarTablero proc near
                         
                          mov    ah,01h
                          int    21h
-                         cmp    al, 83
-                         je     escribirTableroNuevo
+                         cmp    al, 89
+                         je     crearPartida
                          jmp    errorSobreEcribir
 
                          
@@ -134,6 +102,8 @@ verificarTablero proc near
                          mov    ax,4C00h
                          int    21h
 verificarTablero endp
+
+
 
     main:                mov    ax, ds
                          mov    es, ax
